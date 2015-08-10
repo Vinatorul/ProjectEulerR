67d1c017efb038d4588dde9ce49843382a391211
Alexander Kuvaev <alexander@kuvaev.me>
2015-08-10 23:31:20 +0300

    now it can build and run code itself

--- src/problem.rs
+++ src/problem.rs
@@ -1,12 +1,14 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::process::Command;
+
+const PROBLEMS_DIR: &'static str = "problems";
 
 struct Problem {
-	std_id: String,
+	str_id: String,
 	statement: Vec<String>,
 	name: Vec<String>,
 	authors: Vec<String>,
-	input: Vec<String>
 }
 
 enum State {
@@ -14,53 +16,61 @@ enum State {
 	Name,
 	Statement,
 	Authors,
-	Input
 }
 
 impl Problem {
-	fn new(std_id: String) -> Problem {
-		let reader = match File::open(format!("problems/{}.rs", std_id)) {
+	fn new(str_id: String) -> Problem {
+		let reader = match File::open(format!("{}/{}/info.txt", PROBLEMS_DIR, str_id)) {
 			Ok(file) => file,
-			Err(err) => panic!("Unable to load problem '{}`: {}", std_id, err),
+			Err(err) => panic!("Unable to load problem '{}`: {}", str_id, err),
 		};
 		let buf = BufReader::new(&reader);
 		let mut statement = vec![];
 		let mut name = vec![];
 		let mut authors = vec![];
-		let mut input = vec![];
 		let mut state = State::Default;
-		let mut info = false;
 		for line in buf.lines()  {
 			let l = line.unwrap();
-			if !info {
-				if l == "/* [info]" {
-					info = true;
-				}
-				continue;
-			}
 			match l.as_ref() {
 				"[authors]" => state = State::Authors,
 				"[name]" => state = State::Name,
 				"[statement]" => state = State::Statement,
-			 	"[input]" => state = State::Input,
-				"[end] */" => break,
+				"[end]" => break,
 				_ => {
 					match state {
 						State::Authors => authors.push(l.trim().to_string()),
 						State::Name => name.push(l.trim().to_string()),
 						State::Statement => statement.push(l.trim().to_string()),
-						State::Input => input.push(l.trim().to_string()),
 						_ => continue
 					}
 				}
 			}
 		}
 		Problem {
-			std_id: std_id,
+			str_id: str_id,
 			statement: statement,
 			name: name,
 			authors: authors,
-			input: input,
+		}
+	}
+
+	pub fn run(&self) {
+		let builder = Command::new("rustc")
+			.arg(format!("{}/{}/{}.rs", PROBLEMS_DIR, self.str_id, self.str_id))
+			.arg("--out-dir")
+			.arg(format!("{}/{}", PROBLEMS_DIR, self.str_id))
+			.output()
+			.unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
+
+		if builder.status.success() {
+			let problem = Command::new(format!("{}/{}/{}", PROBLEMS_DIR, self.str_id, self.str_id))
+				.output()
+				.unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
+			let s = String::from_utf8_lossy(&problem.stdout);
+			println!("{}", s);
+		} else {
+			let s = String::from_utf8_lossy(&builder.stderr);
+			panic!("rustc failed to build problem {}:\n{}", self.str_id, s);
 		}
 	}
 }
@@ -68,9 +78,9 @@ impl Problem {
 #[test]
 fn read_test() {
 	let p = Problem::new("test".to_string());
-	assert_eq!(p.std_id, "test");
+	assert_eq!(p.str_id, "test");
 	assert_eq!(p.authors, vec!["Testhor", "qwerty"]);
 	assert_eq!(p.name, vec!["Test name", "check"]);
 	assert_eq!(p.statement, vec!["Test statement", "3", "4"]);
-	assert_eq!(p.input, vec!["1sd", "check", "123"]);
+	p.run();
 }
