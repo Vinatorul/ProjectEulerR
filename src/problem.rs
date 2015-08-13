1aaae06614971e1dcc5589341b4f448f81163724
Alexander Kuvaev <alexander@kuvaev.me>
2015-08-14 00:40:47 +0300

    proper error handling

--- src/problem.rs
+++ src/problem.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::process::{Command, Stdio};
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::error::Error;
+use std::fmt;
 
 const PROBLEMS_DIR: &'static str = "problems";
 const INPUT_DIR: &'static str = "input";
@@ -19,6 +21,45 @@ enum State {
 	Name,
 	Statement,
 	Authors,
+}
+
+struct ProblemError {
+	sys_part: String,
+	reason: &'static str,
+	problem_strid: String,
+	test_no: i32,
+}
+
+impl Error for ProblemError {
+	fn description(&self) -> &str {
+        self.reason
+    }
+}
+
+impl fmt::Display for ProblemError {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		if self.test_no >= 0 {
+			write!(f, "{} on problem {} at test {}: {}", self.reason, self.problem_strid, 
+				self.test_no, self.sys_part)
+		}
+		else {
+			write!(f, "{} on problem {}: {}", self.reason, self.problem_strid, 
+				self.sys_part)
+		}
+	}
+}
+
+impl fmt::Debug for ProblemError {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		if self.test_no >= 0 {
+			write!(f, "{} on problem {} at test {}: {}", self.reason, self.problem_strid, 
+				self.test_no, self.sys_part)
+		}
+		else {
+			write!(f, "{} on problem {}: {}", self.reason, self.problem_strid, 
+				self.sys_part)
+		}
+	}
 }
 
 impl Problem {
@@ -48,8 +89,8 @@ impl Problem {
 					}
 				}
 			},
-			Err(err) => println!("Unable to load problem \"{}\" info: {}", str_id, err),
-		};
+			Err(err) => panic!("Unable to load problem \"{}\" info: {}", str_id, err),
+		};	
 		Problem {
 			str_id: str_id,
 			statement: statement,
@@ -57,49 +98,75 @@ impl Problem {
 			authors: authors,
 		}
 	}
-
-	pub fn run(&self) {
-		let builder = Command::new("rustc")
+	pub fn run(&self) -> Result<(), ProblemError> {
+		let builder = match Command::new("rustc")
 			.arg(format!("{}/{}/{}.rs", PROBLEMS_DIR, self.str_id, self.str_id))
 			.arg("--out-dir")
 			.arg(format!("{}/{}", PROBLEMS_DIR, self.str_id))
-			.output()
-			.unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
+			.output() {
+				Ok(cmd) => cmd,
+				Err(e) => return self.make_error("failed to execute rustc", e.to_string(), -1),
+			};
 
-		if builder.status.success() {
-			self.run_test(1);	
-		} else {
+		if !builder.status.success() {
 			let s = String::from_utf8_lossy(&builder.stderr);
-			panic!("rustc failed to build problem {}:\n{}", self.str_id, s);
+			return self.make_error("rustc failed to build problem", s.into_owned(), -1)	
 		}
+		return self.run_test(1);
 	}
 
-	fn run_test(&self, test_no: u32) {
-		let mut input = File::open(format!("{}/{}/{}/{}.txt", PROBLEMS_DIR, self.str_id, INPUT_DIR, test_no))
-			.unwrap_or_else(|e| { panic!("failed to load test \"{}\": {}", test_no, e) });
-		let mut s = String::new(); // passing throw String coz as_slice still unstable
-		input.read_to_string(&mut s)
-			.unwrap_or_else(|e| { panic!("couldn't read test \"{}\" file: {}", test_no, e) });
+	fn run_test(&self, test_no: i32) -> Result<(), ProblemError> {
+		let mut input = match File::open(format!("{}/{}/{}/{}.txt", 
+			PROBLEMS_DIR, self.str_id, INPUT_DIR, test_no)) {
+			Ok(file) => file,
+			Err(e) => return self.make_error("failed to load test", e.to_string(), test_no),
+		};
 
-		let problem = Command::new(format!("{}/{}/{}", PROBLEMS_DIR, self.str_id, self.str_id))
+		let mut s = String::new(); // passing throw String coz as_slice still unstable
+		let result = input.read_to_string(&mut s);
+		if result.is_err() {
+			return self.make_error("couldn't read test", result.err().unwrap().to_string(), test_no);
+		}
+
+		let problem = match Command::new(format!("{}/{}/{}", PROBLEMS_DIR, self.str_id, self.str_id))
 			.stdin(Stdio::piped())
 			.stdout(Stdio::piped())
-			.spawn()
-			.unwrap_or_else(|e| { panic!("failed to execute test \"{}\": {}", test_no, e) });
+			.spawn() {
+				Ok(cmd) => cmd,
+				Err(e) => return self.make_error("failed to execute test", e.to_string(), test_no),	
+			};
 		
-		problem.stdin.unwrap().write_all(s.as_bytes())
-			.unwrap_or_else(|e| { panic!("couldn't write stdin test \"{}\": {}", test_no, e) });
-		{
-			let mut s = String::new(); // passing throw String coz as_slice still unstable
-			problem.stdout.unwrap().read_to_string(&mut s)
-				.unwrap_or_else(|e| { panic!("couldn't read stdout test \"{}\": {}", test_no, e) });
-
-			let mut output = File::create(format!("{}/{}/{}/{}.txt", PROBLEMS_DIR, self.str_id, OUTPUT_DIR, test_no))
-				.unwrap_or_else(|e| { panic!("failed to save test \"{}\" output: {}", test_no, e) }); 
-
-			output.write_all(s.as_bytes())
-				.unwrap_or_else(|e| { panic!("couldn't write test \"{}\" answer: {}", test_no, e) });
+		let result = problem.stdin.unwrap().write_all(s.as_bytes());
+		if result.is_err() {
+			return self.make_error("couldn't send stdin to test", result.err().unwrap().to_string(), test_no);
 		}
+
+		let mut s = String::new(); // passing throw String coz as_slice still unstable
+		let result = problem.stdout.unwrap().read_to_string(&mut s);
+		if result.is_err() {
+			return self.make_error("couldn't read stdout from test", result.err().unwrap().to_string(), test_no);
+		}
+
+		let mut output = match File::create(format!("{}/{}/{}/{}.txt", 
+			PROBLEMS_DIR, self.str_id, OUTPUT_DIR, test_no)) {
+			Ok(file) => file,
+			Err(e) => return self.make_error("failed to create test output file", e.to_string(), test_no),
+		};
+
+		let result = output.write_all(s.as_bytes());
+		if result.is_err() {
+			return self.make_error("couldn't write test answer", result.err().unwrap().to_string(), test_no);
+		}
+		Ok(())
+	}
+
+	fn make_error(&self, reason: &'static str, sys_part: String, test_no: i32) -> Result<(), ProblemError> {
+		Err(ProblemError { 
+			reason:reason, 
+			sys_part: sys_part, 
+			problem_strid: self.str_id.clone(), 
+			test_no: test_no
+		})
 	}
 }
 
@@ -110,5 +177,8 @@ fn read_test() {
 	assert_eq!(p.authors, vec!["Testhor", "qwerty"]);
 	assert_eq!(p.name, vec!["Test name", "check"]);
 	assert_eq!(p.statement, vec!["Test statement", "3", "4"]);
-	p.run();
+	let result = p.run();
+	if result.is_err() {
+		panic!(result.err().unwrap().to_string());
+	} 
 }
